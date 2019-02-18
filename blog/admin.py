43b9e418198fcbeffa85ba9c22bbbdfe30ab8cfc
Jungmi <rhythmof@gmail.com>
2019-02-18 21:02:54 +0900

    admin.py수정

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Post
 from .models import Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+# admin에서 모든 model을 넣을 필요는 없음. admin에서 관리하고싶은 것만 등록
+# CRUD 중에 고를 수 있음
+# admin.site.register(Post)
+# admin.site.register(Comment) #기본 ModelAdmin 으로 동작
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
