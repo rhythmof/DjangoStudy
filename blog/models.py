4b42337e6c8724102463036c4b555aa3c39bee2f
Jungmi <rhythmof@gmail.com>
2019-02-18 09:32:08 +0900

    Comment Table

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,9 @@ class Post(models.Model):
     content=models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    auth_name = models.CharField(max_length=10)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
