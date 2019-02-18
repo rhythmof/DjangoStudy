ea8ecf835ceb485f41cdebc358c123034012e0f5
Jungmi <rhythmof@gmail.com>
2019-02-18 21:29:25 +0900

    Tag Default ADD

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content=models.TextField()
-    tags = models.CharField(max_length=10)
+    tags = models.CharField(max_length=10, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
